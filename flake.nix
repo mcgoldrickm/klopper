71a5916711077c31d691d438032b1ae67ce47546
Michael McGoldrick <michael.mcgoldrick@gmail.com>
2025-01-03 14:30:54 +0000

    correct board for rpi5 - hopefully

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         # bcm2712 for rpi 5
         # See the docs at:
         # https://www.raspberrypi.com/documentation/computers/linux_kernel.html#native-build-configuration
-        raspberry-pi-nix.board = "bcm2711";
+        raspberry-pi-nix.board = "bcm2712";
         time.timeZone = "Europe/London";
 
         # Select internationalisation properties.
